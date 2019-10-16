74d0841079b9e335094f1fb0945c7c30f702694d
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-10-16 23:48:41 +0200

    Fibonacci & faculteit

---
+++ Opdrachten/faculteit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_faculteit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            for(int i=(number-1); i>0; i--){
+                number*=i;
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(input + "! = " + number);
+        }
+    }
+}
---
+++ Opdrachten/fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3_fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How long would you like the row?");
+            String input = Console.ReadLine();
+            Console.WriteLine("-");
+            int n = Convert.ToInt32(input);
+            int tmpone = 0;
+            int tmptwo = 1;
+            int current;
+            Console.WriteLine(0);
+            Console.WriteLine(1);
+            for (int i = 0; i < n; i++)
+            {
+                current = tmpone + tmptwo;
+                tmpone = tmptwo;
+                tmptwo = current;
+
+                Console.WriteLine(current);
+            }
+        }
+    }
+}
