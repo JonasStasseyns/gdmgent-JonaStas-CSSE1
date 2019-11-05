f24c3959c2bf464875f47166802844e6bac1a21b
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-05 17:18:47 +0100

    som + verschil

---
+++ Opdrachten/Opdracht5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Opdracht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void addNumbers(int num1, int num2){
+                int sum = num1 + num2;
+                Console.WriteLine(' ');
+                Console.WriteLine("SOM: " + num1 + " + " + num2 + " = " + sum);
+                Console.WriteLine(' ');
+            }
+
+            void subtractNumbers(int num1, int num2){
+                int sum = num1 - num2;
+                Console.WriteLine(' ');
+                Console.WriteLine("VERSCHIL: " + num1 + " - " + num2 + " = " + sum);
+                Console.WriteLine(' ');
+            }
+
+            addNumbers(4, 5);
+            subtractNumbers(10, 6);
+        }
+
+        
+    }
+}
