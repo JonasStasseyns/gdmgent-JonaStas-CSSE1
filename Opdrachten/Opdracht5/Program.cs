82b8f22e513841ffee623cf15b1f52323ca0c3a8
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-05 17:28:46 +0100

    quotient en product

--- Opdrachten/Opdracht5/Program.cs
+++ Opdrachten/Opdracht5/Program.cs
@@ -12,16 +12,29 @@ namespace Opdracht5
                 Console.WriteLine("SOM: " + num1 + " + " + num2 + " = " + sum);
                 Console.WriteLine(' ');
             }
-
             void subtractNumbers(int num1, int num2){
-                int sum = num1 - num2;
+                int substraction = num1 - num2;
                 Console.WriteLine(' ');
-                Console.WriteLine("VERSCHIL: " + num1 + " - " + num2 + " = " + sum);
+                Console.WriteLine("VERSCHIL: " + num1 + " - " + num2 + " = " + substraction);
+                Console.WriteLine(' ');
+            }
+            void multiplyNumbers(int num1, int num2){
+                int multiplied = num1 * num2;
+                Console.WriteLine(' ');
+                Console.WriteLine("PRODUCT: " + num1 + " x " + num2 + " = " + multiplied);
+                Console.WriteLine(' ');
+            }
+            void divideNumbers(int num1, int num2){
+                int quotient = num1 / num2;
+                Console.WriteLine(' ');
+                Console.WriteLine("QUOTIENT: " + num1 + " / " + num2 + " = " + quotient);
                 Console.WriteLine(' ');
             }
 
             addNumbers(4, 5);
             subtractNumbers(10, 6);
+            multiplyNumbers(3, 6);
+            divideNumbers(20, 4);
         }
 
         
